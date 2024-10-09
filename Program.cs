1d9e426ef6ba8a2a3524030a4bd13fd49e26817a
Shahad <Shahadalnaamani@outlook.com>
2024-10-09 13:10:21 +0400

    Added functionality to program and added Random class and added functionality to it

--- Program.cs
+++ Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Lets play football!");
+            //Alligning the tital of the page to the middle of the console 
+            string Welcome = "F O O T B A L L   G A M E   S I M U L A T O R";
+            Console.SetCursorPosition((Console.WindowWidth - Welcome.Length) /2, Console.CursorTop); 
+            Console.WriteLine(Welcome);
+
+            //Getting user input for the team names 
+            Console.Write("\nEnter name for Team 1: "); //write and not writeline so that user will input on the same line as text 
+            string Team1 = Console.ReadLine();
+            Console.Write("\nEnter name for Team 2: ");
+            string Team2 = Console.ReadLine();
+
+            RandomInt.GetRandom();
+            //Call class and then generate the teams -> generation should be done in team class 
         }
     }
 }
---
+++ Ramdom.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FootBallGameSimulator
+{
+    //Generates random numbers 
+	public static class RandomInt
+	{
+        //Method overloading used so that we can carry out the same function (getting random number) in a different way so that all parts of the system can use the same class 
+        
+        
+        public  static int GetRandom() 
+        {
+            Random random = new Random();
+            int randomNumber = random.Next();
+            Console.WriteLine("Random number " + randomNumber);
+            return randomNumber;
+        }
+
+        
+        public static int GetRandom(int Min, int Max) //This override will be used to get a random Skill level 
+        {
+            Random random = new Random();
+            int randomInRange = random.Next(Min, Max);
+
+            
+            Console.WriteLine("Random number between 1 and 99: " + randomInRange);
+            return randomInRange;
+        }
+    }
+}
