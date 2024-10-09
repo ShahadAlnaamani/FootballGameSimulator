2a80bfbd26053e6e788f7d64b7ff9dbba65e81c5
Shahad <Shahadalnaamani@outlook.com>
2024-10-09 11:22:12 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FootBallGameSimulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets play football!");
+        }
+    }
+}
